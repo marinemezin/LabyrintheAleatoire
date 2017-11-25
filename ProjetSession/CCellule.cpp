274bf3cae2c43af2ce159f3797d6091bceca462d
Elodie TOROSSIAN <elodie110@yahoo.fr>
2017-11-26 00:01:44 +0100

    j

--- ProjetSession/CCellule.cpp
+++ ProjetSession/CCellule.cpp
@@ -18,14 +18,17 @@ CCellule::~CCellule(){
 
 void CCellule::modifmurhaut() {
 	murhaut = false;
-
+	plateau.detruiremurbas(ligne + 1, colonne);
 }
 void CCellule::modifmurbas() {
-
+	murbas = false;
+	plateau.detruiremurhaut(ligne - 1, colonne);
 }
 void CCellule::modifmurdroit() {
-
+	murdroit = false;
+	plateau.detruiremurgauche(ligne, colonne + 1);
 }
 void CCellule::modifmurgauche() {
-
+	murgauche = false;
+	plateau.detruiremurdroit(ligne, colonne - 1);
 }
--- ProjetSession/CPlateau.h
+++ ProjetSession/CPlateau.h
@@ -4,8 +4,16 @@ class CPlateau
 {
 private : 
 	CCellule plateau[10][15];
+	int ligdep;
+	int coldep;
+	int ligarr;
+	int colarr;
 
 
 public : 
+	void detruiremurbas(int ligne, int colonne);
+	void detruiremurhaut(int ligne, int colonne);
+	void detruiremurdroit(int ligne, int colonne);
+	void detruiremurgauche(int ligne, int colonne);
 	
 };
